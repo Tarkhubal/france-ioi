c2336235f2ff54dd40a94ca056bba2a242b093b1
The_WildThomas <78657181+Tarkhubal@users.noreply.github.com>
2021-12-13 16:57:35 +0100

    Create Niveau1_Chapitre3.py

---
+++ Niveau1_Chapitre3.py
@@ -0,0 +1,16 @@
+#Réponds
+print(42)
+
+
+#L'éclipse
+print(12581 - 11937)
+
+
+#Bonbons pour tout le monde !
+print((25 + 30 + 27 + 22 - 8) * 3)
+
+
+#L'algoréathlon
+print(2 + 34 + 6, end = " ")
+print((2 + 34 + 6) * 2, end = " ")
+print((2 + 34 + 6) * 3, end = " ")
