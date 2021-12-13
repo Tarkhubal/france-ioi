137f3a4602727942de2b726facdd0514de97fb4b
The_WildThomas <78657181+Tarkhubal@users.noreply.github.com>
2021-12-13 16:02:05 +0100

    Create niveau1_Chapitre2.py

---
+++ niveau1_Chapitre2.py
@@ -0,0 +1,30 @@
+#Punition
+for loop in range(135):
+   print("Je dois respecter le Grand Sorcier.")
+
+
+#Mathématiques de base
+for loop in range(13):
+   print("9 * 8 = 72")
+
+
+#Transport d'eau
+from robot import *
+
+for loop in range(2):
+   gauche()
+print("Bonjour, laissez-moi vous aider")
+ramasser()
+for loop in range(32):
+   droite()
+deposer()
+
+
+#Le secret du Goma
+from robot import *
+
+for loop in range(15):
+   droite()
+   ramasser()
+droite()
+deposer()
