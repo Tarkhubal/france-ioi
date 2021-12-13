b27c81294055f2c2e9541dd01d9b55a8885ea3ed
The_WildThomas <78657181+Tarkhubal@users.noreply.github.com>
2021-12-13 15:56:12 +0100

    Create niveau1_Chapitre1.py

---
+++ niveau1_Chapitre1.py
@@ -0,0 +1,29 @@
+#Hello World!
+print("Hello world!")
+
+
+#Présentation
+print("Coucou !")
+print("Je m'appelle Camthalion")
+print("Ma devise est 'Parler peu mais parler bien'.")
+
+
+#Plan de la montagne
+print("Tout droit tu grimperas,")
+print("La clé tu trouveras,")
+print("Habile tu seras,")
+print("Quand tu les porteras,")
+print("Et avec le chef tu reviendras !")
+
+
+#Dans le fourré
+from robot import *
+
+haut()
+haut()
+haut()
+droite()
+droite()
+bas()
+bas()
+droite()
