e4aa31d4770aed69cb99937b4f6c8ed13b2822fb
The_WildThomas <78657181+Tarkhubal@users.noreply.github.com>
2021-12-13 16:51:18 +0100

    Update niveau1_Chapitre2.py

--- niveau1_Chapitre2.py
+++ niveau1_Chapitre2.py
@@ -28,3 +28,79 @@ for loop in range(15):
    ramasser()
 droite()
 deposer()
+
+
+#Sisyphe
+from robot import *
+for loop in range(21):
+  haut()
+  droite()
+for loop in range(21):
+  gauche()
+  bas()
+
+
+#Page d'écriture
+for loop in range(30):
+   print("a_", end = "")
+print()
+for loop in range(30):
+   print("b_", end = "")
+print()
+for loop in range(30):
+   print("c_", end = "")
+
+
+#Jeu de dames
+for loop in range(20):
+   for loop in range(20):
+      print("OX", end = "")
+   print()
+   for loop in range(20):
+      print("XO", end = "")
+   print()
+
+
+#Mont Kailash
+from robot import *
+for loop in range(108):
+   for loop in range(13):
+      haut()
+   for loop in range(13):
+      droite()
+   for loop in range(13):
+      bas()
+   for loop in range(13):
+      gauche()
+
+
+#Vendanges
+from robot import *
+
+for loop in range(20):
+   ramasser()
+   for loop in range(15):
+      droite()
+   deposer()
+   for loop in range(15):
+      gauche()
+
+
+#Le Grand Événement
+from robot import *
+
+haut()
+for loop in range(4):
+   for loop in range(8):
+      haut()
+   droite()
+   for loop in range(8):
+      bas()
+   droite()
+for loop in range(8):
+   haut()
+droite()
+for loop in range(9):
+   bas()
+for loop in range(9):
+   gauche()
