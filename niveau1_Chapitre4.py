ea9a2024965fa78db81c97f247a6b10433a841a2
The_WildThomas <78657181+Tarkhubal@users.noreply.github.com>
2021-12-14 11:48:16 +0100

    Update niveau1_Chapitre4.py

--- niveau1_Chapitre4.py
+++ niveau1_Chapitre4.py
@@ -16,3 +16,31 @@ print(âgeAîné - âgeCadet)
 nbRep = int(input())
 for loop in range(nbRep):
    print("Je dois suivre en cours")
+
+
+#Graduation de thermomètres
+tempMin = int(input())
+tempMax = int(input())
+temperature = tempMin
+for loop in range(tempMax - tempMin + 1):
+   print(temperature)
+   temperature += 1
+   
+   
+#Jeu de calcul mental
+nbChef = int(input())
+étape = 1
+calcul = 66
+for loop in range(nbChef):
+   calcul *= étape
+   étape += 1
+   print(calcul)
+
+
+#La Grande Braderie
+position = int(input())
+largeurEmplacement = int(input())
+
+for iVendeur in range(int(input()) + 1):
+   print(position)
+   position += largeurEmplacement
