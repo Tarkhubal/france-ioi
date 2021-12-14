f8afb0a8b32e8eee9da575aac5a130988d1aca80
The_WildThomas <78657181+Tarkhubal@users.noreply.github.com>
2021-12-14 11:45:58 +0100

    Update niveau1_Chapitre4.py

--- niveau1_Chapitre4.py
+++ niveau1_Chapitre4.py
@@ -4,3 +4,15 @@ print((int(input())**2) * 23)
 
 #Retraite spirituelle
 print((60**2) * 16 * int(input()))
+
+
+#Âge des petits-enfants
+âgeCadet = int(input())
+âgeAîné = int(input())
+print(âgeAîné - âgeCadet)
+
+
+#Encore des punitions
+nbRep = int(input())
+for loop in range(nbRep):
+   print("Je dois suivre en cours")
