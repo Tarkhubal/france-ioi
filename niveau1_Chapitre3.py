c146c6200e0d2c5a6fe9358d8f9472dd81ab1532
The_WildThomas <78657181+Tarkhubal@users.noreply.github.com>
2021-12-14 11:32:28 +0100

    Update niveau1_Chapitre3.py

--- niveau1_Chapitre3.py
+++ niveau1_Chapitre3.py
@@ -14,3 +14,9 @@ print((25 + 30 + 27 + 22 - 8) * 3)
 print(2 + 34 + 6, end = " ")
 print((2 + 34 + 6) * 2, end = " ")
 print((2 + 34 + 6) * 3, end = " ")
+
+
+#Cour de récréation
+longueur = 17 * 5 + 7 * 2 + 5 * 1 + 2 * 2
+print(longueur * longueur)
+print(longueur * 4)
