d105f03fd5febf7979e720cd5710df2bb63bdb64
The_WildThomas <78657181+Tarkhubal@users.noreply.github.com>
2022-01-02 20:56:21 +0100

    Update niveau1_Chapitre1.py

--- niveau1_Chapitre1.py
+++ niveau1_Chapitre1.py
@@ -1,11 +1,21 @@
 #Hello World!
 print("Hello world!")
 
+  #Programme sous forme "française" :
+
+    # écrire "Hello world!"   puis sauter une ligne
+
 
 #Présentation
 print("Coucou !")
 print("Je m'appelle Camthalion")
 print("Ma devise est 'Parler peu mais parler bien'.")
+
+  #Programme sous forme "française" :
+
+    # écrire "Coucou !"                                       puis sauter une ligne
+    # écrire "Je m'appelle Camthalion"                        puis sauter une ligne
+    # écrire "Ma devise est 'Parler peu mais parler bien'."   puis sauter une ligne
 
 
 #Plan de la montagne
@@ -14,6 +24,12 @@ print("La clé tu trouveras,")
 print("Habile tu seras,")
 print("Quand tu les porteras,")
 print("Et avec le chef tu reviendras !")
+
+  #Programme sous forme "française" :
+
+    # écrire "Tout droit tu grimperas,"          puis sauter une ligne
+    # écrire "La clé tu trouveras,"              puis sauter une ligne
+    # écrire "Et avec le chef tu reviendras !"   puis sauter une ligne
 
 
 #Dans le fourré
@@ -27,6 +43,18 @@ droite()
 bas()
 bas()
 droite()
+
+  #Programme sous forme "française" :
+
+    # importer tous les éléments de la bibliothèque "robot" [pour permettre d'exécuter les blocs ci-dessous]
+    # aller en haut
+    # aller en haut
+    # aller en haut
+    # aller à droite
+    # aller à droite
+    # aller en bas
+    # aller en bas
+    # aller à droite
 
 
 #Empilement de cylindres
@@ -60,6 +88,3 @@ vider(5)
 transferer(3, 5)
 remplir(3)
 transferer(3, 5)
-
-
-# {END}
